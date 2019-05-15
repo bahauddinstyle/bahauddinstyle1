24c1203bf6209a64904ad7eebdccb97b2d89962a
bahauddinstyle <50423092+bahauddinstyle@users.noreply.github.com>
2019-05-16 02:39:20 +0700

    Ggfff
    Gd Dfff

---
+++ Bahauddinstyle1.sh
@@ -0,0 +1,286 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"#######################################################"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     bahauddin tayu pati jateng"$i"                              "
+echo $i" |"$me"#############################################################"$i"|"
+echo $i" |"$pu" AUTHOR :"$k" bahauddin ganteng"$i"                         "
+echo $i" |"$pu" FACEBOOK :"$ku" bahauddin tayu pati"$i"                   "
+echo $i" |"$pu" FROM :"$cy"~ BENDOKATON TAYU PATI JATENG"$i"    "
+echo $i" |"$cy"            ~ SANTAI WAE SLOW WAE YO BRO"$i"            "
+echo $i" |"$pu" WhatsApp :"$ku" 085726833815"$i"              "
+echo $i" |"$pu" Blog :"$ku" http://cybernoobstayupati.blogspot.com/"$i""
+echo $i"["$bi"•"$i"]"$me"#######################################################"$i"["$bi"•"$i"]"
+echo
+echo $i"##########################################   "
+echo $i"|"$me" 1"$i" |"$cy" ULTIMATE DORK       "$i""$i"    "$cy" "$i" "
+echo $i"|"$me" 2"$i" |"$cy" SQLI SCAN    "$i""$i"    "$cy" "$i" "
+echo $i"|"$me" 3"$i" |"$cy" BLACK HYDRA              "$i""$i"      "$cy" "$i" "
+echo $i"|"$me" 4"$i" |"$cy" STRIKER        "$i""$i"    "$cy" "$i" "
+echo $i"|"$me" 5"$i" |"$cy" MELACAK ORANG      "$i""$i"    "$cy" "$i" "
+echo $i"|"$me" 6"$i" |"$cy" SQL SCAN"$i""$i"    "$cy" "$i" "
+echo $i"#########################################"$i"    "$cy" "$i" "
+echo $i"|"$me" 7"$i" |"$cy" SQLMAP  "$i""$i"    "
+echo $i"|"$me" 8"$i" |"$cy" SQLMATE   "$i""$i"    "$cy" "$i" "
+echo $i"|"$me" 9"$i" |"$cy" DDOS LITE     "$i""$i"    "$cy" "$i" "
+echo $i"|"$me"10"$i" |"$cy" PRIVATE FACEBOOK   "$i""$i"    "$cy" "$i" "
+echo $i"|"$me"11"$i" |"$cy" HACK INSTAGRAM     "$i""$i"    "$cy" "$i" "
+echo $i"|"$me"12"$i" |"$cy" DARK FLY       "$i""$i"    "$cy" "$i" "
+echo $i"|"$me"13"$i" |"$cy" KALSEL TOOL       "$i""$i"    "$cy" "$i" "
+echo $i"#########################################"$i"    "$cy" "$i" "
+echo $i"|"$me"14"$i" |"$cy" FBR FACEBOOK      "$i""$i"    "$cy" "$i" "
+echo $i"|"$me"15"$i" |"$cy" STARK2.0   "$i""$i"    "
+echo $i"|"$me"16"$i" |"$cy" TOOL VIP      "$i""$i"    "$cy" "$i" "
+echo $i"|"$me"17"$i" |"$cy" TOOL TMX        "$i""$i"    "$cy" "$i" "
+echo $i"#########################################"$i"    "$cy" "$i" "
+echo $i"|"$me"18"$i" |"$cy" LAZYMUX  "$i""$i"    "$cy" "$i" "
+echo $i"|"$me"19"$i" |"$cy" BAHAN INSTALL     "$i""$i"    "$cy" "$i" "
+echo $i"|"$me"20"$i" |"$cy" BAHAN INSTALL     "$i""$i"    "$cy" "$i" "
+echo $i"#########################################    "
+echo
+echo $me"┌===>"$bi"["$i"Bahauddin"$bi"]"$me"======"$bi"["$i""monggo di Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/jaxBCD/Ultimate-Dork.git
+cd Ultimate-Dork
+pip2 install -r requirements.txt
+python ultimate-dork.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+apt install curl
+git clone https://github.com/thelinuxchoice/sqliscan/
+cd sqliscan
+bash sqliscan.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Gameye98/Black-Hydra/b-hydra/
+cd b-hydra
+mv blackhydra.py main.py
+chmod +x main.py
+python2 main.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/UltimateHackers/Striker.git
+cd Striker
+pip2 install -r requirements.txt
+python2 striker.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/thelinuxchoice/userrecon
+cd userrecon
+bash userrecon.sh
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://www.github.com/Cvar1984/sqlscan
+cd sqlscan
+chmod +x sqlscan.php
+php sqlscan.php
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+pip2 install mechanize bs4 HTMLparser argparse requests urlparse2
+git clone https://github.com/UltimateHackers/sqlmate
+cd sqlmate
+chmod +x sqlmate
+python2 sqlmate
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/PangeranAlvins/Repot3
+cd Report3
+python2 Repot3.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/WahyuHack/ighack
+cd ighack
+python2 hack.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+chmod +x*
+python2 install.py
+DarkFly
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/KALSELeXploit/KALSEL-Z-TOOL
+cd KALSEL-Z-TOOL
+sh Z-TOOL.sh
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Cabdulahi/fbr
+cd fbr
+bash install.sh
+python2 fbr.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/aniketstark/STARK2.0
+cd STARK2.0
+chmod +x install
+bash install
+python2 stark.py
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/Aditya021/tool-vip
+cd tool-vip
+sh tool-vip.sh
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/MoMuilWH/TmX-FW
+cd TmX-FW
+chmod +x *
+python2 TmX-FW.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Gameye98/Lazymux 
+cd Lazymux 
+chmod +X* 
+python2 lazymux.py
+fi
+
+if [ $pil = 19 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " SAMPEAN S U K S E S "|lolcat
+fi
+
+
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"MATURSUWUN WES NGANGGO TOOLKU"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $ku"WhatsApp :"$i" 085726833815"
+echo $ku"Facebook :"$i" BAHAUDDIN"
+sleep 2
+echo $pur">> Thanks Yang Sudah Support Saya <<"
+exit
+fi
