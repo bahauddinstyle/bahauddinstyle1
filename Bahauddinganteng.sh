8b0ec0126dea7fa09a771058646910da2ab950c4
bahauddinstyle <50423092+bahauddinstyle@users.noreply.github.com>
2019-05-16 23:00:50 +0700

    Hggg
    Hgggg

---
+++ Bahauddinganteng.sh
@@ -0,0 +1,533 @@
+#Bahauddin Tool Hacking
+#CODED BY : Bahauddin
+#CODENAME : Bahauddin Tool Hacking
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo "-----------------------------------------------------------"
+toilet -f pagga " Bahauddin Tool Hacking "|lolcat
+echo "-----------------------------------------------------------"
+echo $me"[+]─────────────────────────────────────────────────────[+]"
+echo $me" | ••••••••••       BAHAUDDIN TOOL HACKING     ••••••••••••  |"
+echo $me" | ───────────────────────────────────────────────────── |"
+echo $me" | NAMA : Bahauddin                        "
+echo $me" | From : Bendokaton Tayu Pati jateng                         "
+echo $me" | Facebook : Bahauddin Tayu Pati                            "
+echo $pu" | Blogku   : http://cybernoobstayupati.blogspot.com/                       "
+echo $pu" | Nomer Hp : 085726833815 atau 089647602435              "
+echo $pu" | Team: PATI CYBER TEAM          "
+echo $pu" | NB  : GUNAKAN DG BIJAK               "
+echo $pu" | SANTAI WAE ... SLOW WAE                    "
+echo $pu"[+]─────────────────────────────────────────────────────[+]"
+echo
+echo $me"##########################################   "
+echo $me"#"$me" 1"$i" |"$cy" ULTIMATE DORK       "$i""$i"    "$cy" "$i" "
+echo $me"#"$me" 2"$i" |"$cy" SQLI SCAN    "$i""$i"    "$cy" "$i" "
+echo $me"#"$me" 3"$i" |"$cy" BLACK HYDRA              "$i""$i"      "$cy" "$i" "
+echo $me"#"$me" 4"$i" |"$cy" STRIKER        "$i""$i"    "$cy" "$i" "
+echo $me"#"$me" 5"$i" |"$cy" MELACAK ORANG      "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me" 6"$i" |"$cy" SQL SCAN"$i""$i"    "$cy" "$i" "
+echo $ku"#"$me" 7"$i" |"$cy" SQLMAP  "$i""$i"    "
+echo $ku"#"$me" 8"$i" |"$cy" KK KTP GENERATE   "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me" 9"$i" |"$cy" DDOS LITE     "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"10"$i" |"$cy" PRIVATE FACEBOOK   "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"11"$i" |"$cy" HACK INSTAGRAM     "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"12"$i" |"$cy" DARK FLY       "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"13"$i" |"$cy" KALSEL TOOL       "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"14"$i" |"$cy" FBR FACEBOOK      "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"15"$i" |"$cy" STARK2.0   "$i""$i"    "
+echo $me"#"$me"16"$i" |"$cy" TOOL VIP      "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"17"$i" |"$cy" TOOL TMX        "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"18"$i" |"$cy" LAZYMUX  "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"19"$i" |"$cy" HACK FB 1     "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"20"$i" |"$cy" HACK FB 2     "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"21"$i" |"$cy"  HACK FB 3      "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"22"$i" |"$cy"  HACK FB MBF      "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"23"$i" |"$cy"  HACK FB OK     "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"24"$i" |"$cy"  HACK FB ROOT     "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"25"$i" |"$cy"  YAHOO CLONING FB     "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"26"$i" |"$cy"  PROFILE GUARD FB     "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"27"$i" |"$cy"  REPORT FACEBOOK      "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"28"$i" |"$cy"  AUTO REACTION FB    "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"29"$i" |"$cy"  INFORMATION FB     "$i""$i"    "$cy" "$i" "
+echo $ij"#"$me"30"$i" |"$cy"  BOT KOMEN FB      "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"31"$i" |"$cy"   AUTOLIKE FB     "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"32"$i" |"$cy"  PHISING FB 1      "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"33"$i" |"$cy"  PHISING FB 2      "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"34"$i" |"$cy"  PHISING FB 3      "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"35"$i" |"$cy"  PHISING FB 4      "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"36"$i" |"$cy"  TEMBAK KUOTA     "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"37"$i" |"$cy"  SCRIPT DEFACE CREATOR    "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"38"$i" |"$cy"  DEFACE 1    "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"39"$i" |"$cy"  DDOS    "$i""$i"    "$cy" "$i" "
+echo $ku"#"$me"40"$i" |"$cy"  ADMIN LOGIN   "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"41"$i" |"$cy"  BAHAN INSTALL      "$i""$i"    "$cy" "$i" "
+echo $me"#"$me"42"$i" |"$cy"  KANGGO METU      "$i""$i"    "$cy" "$i" "
+echo $pu"#########################################    "
+echo
+echo $me"┌===>"$pu"["$i"Bahauddin"$bi"]"$me"======"$ku"["$ij""monggo di Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/jaxBCD/Ultimate-Dork.git
+cd Ultimate-Dork
+pip2 install -r requirements.txt
+python ultimate-dork.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+apt install curl
+git clone https://github.com/thelinuxchoice/sqliscan/
+cd sqliscan
+bash sqliscan.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Gameye98/Black-Hydra/b-hydra/
+cd b-hydra
+mv blackhydra.py main.py
+chmod +x main.py
+python2 main.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/UltimateHackers/Striker.git
+cd Striker
+pip2 install -r requirements.txt
+python2 striker.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/thelinuxchoice/userrecon
+cd userrecon
+bash userrecon.sh
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://www.github.com/Cvar1984/sqlscan
+cd sqlscan
+chmod +x sqlscan.php
+php sqlscan.php
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+apt install git php php-cli php-curl php-dev
+git clone https://github.com/ghostrecoded/kkktpgenerate2/
+cd kkktpgenerate2
+php gogenerate.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/PangeranAlvins/Repot3
+cd Report3
+python2 Repot3.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/WahyuHack/ighack
+cd ighack
+python2 hack.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+chmod +x*
+python2 install.py
+DarkFly
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/KALSELeXploit/KALSEL-Z-TOOL
+cd KALSEL-Z-TOOL
+sh Z-TOOL.sh
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Cabdulahi/fbr
+cd fbr
+bash install.sh
+python2 fbr.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/aniketstark/STARK2.0
+cd STARK2.0
+chmod +x install
+bash install
+python2 stark.py
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/Aditya021/tool-vip
+cd tool-vip
+sh tool-vip.sh
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/MoMuilWH/TmX-FW
+cd TmX-FW
+chmod +x *
+python2 TmX-FW.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Gameye98/Lazymux 
+cd Lazymux 
+chmod +X* 
+python2 lazymux.py
+fiaaaaq
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/fbbrute
+cd fbbrute
+php fb.php
+fi
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 21 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+fi
+
+if [ $pil = 22 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 23 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 24 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 25 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 26 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 27 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 28 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+
+if [ $pil = 29 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 30 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $pil = 31 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 32 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 33 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 34 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 35 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 36 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Bintang73/tembaktembakan
+cd tembaktembakan
+python dor.py
+fi
+
+if [ $pil = 37 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+python2 create.py
+fi
+
+if [ $pil = 38 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/Ranginang67/AOXdeface
+cd AOXdeface
+aox.py
+fi
+
+if [ $pil = 39 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/KALSELeXploit/DDoS
+cd DDoS
+python2 ddos.py
+fi
+
+if [ $pil = 40 ]
+then
+clear
+figlet -f slant "Bahauddin"|lolcat
+sleep 1
+git clone https://github.com/KALSELeXploit/AdminFinder
+cd AdminFinder
+python2 admin.py
+fi
+
+
+if [ $pil = 41 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " SAMPEAN S U K S E S "|lolcat
+fi
+
+
+
+if [ $pil = 42 ]
+then
+clear
+figlet -f slant "KELUAR"|lolcat
+sleep 2
+echo $cy"MATURSUWUN WES NGANGGO TOOLKU"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $ku"WhatsApp :"$i" 085726833815"
+echo $ku"Facebook :"$i" Bahauddin Tayu pati"
+sleep 2
+echo $pur">> Thanks Yang Sudah Support Saya <<"
+exit
+fi
